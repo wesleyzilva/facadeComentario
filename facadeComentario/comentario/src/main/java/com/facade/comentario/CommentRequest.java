2c12dadcec5bb9a87502806aaf9ff9c4fd78859b
Silva <wesley.silva@br.experian.com>
2025-09-26 08:01:08 -0300

    Substituindo conteúdo da main com versão local

--- facadeComentario/comentario/src/main/java/com/facade/comentario/CommentRequest.java
+++
@@ -1,8 +0,0 @@
-package com.facade.comentario.model;
-
-/**
- * DTO para receber os dados de criação de um novo comentário.
- * Inclui apenas os campos que o cliente deve fornecer.
- */
-public record CommentRequest(String author, String content) {
-}
